8ec85f1418c55b28f64d42028e3c1e52cabdd0a6
Salman <asalman7875@gmail.com>
2023-01-13 11:18:27 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import Axios from 'axios'
 
-function App() {
+import './App.css'
+
+function App () {
+  const [counrtyName, setCountryName] = useState('')
+  const [data, setData] = useState('')
+
+  const fetchCountry = name => {
+    Axios.get(`https://restcountries.com/v2/name/${name}`).then(res => {
+      console.log(res.data[0])
+      setData(res.data[0])
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='container'>
+        <div className='search'>
+          <input
+            type='text'
+            placeholder='Ex. India'
+            onChange={e => setCountryName(e.target.value)}
+          />
+          <button onClick={() => fetchCountry(counrtyName)}>Search</button>
+        </div>
+
+        <div className='card'>
+          <img src={data?.flag} alt={data?.name} />
+          {data ? (
+            <div className='card-text'>
+              <h3>
+                Country Name: <span>{data?.name}</span>
+              </h3>
+              <h3>
+                Languages: <span>{data?.languages[0].name}</span>
+              </h3>
+              <h3>
+                Currency: <span>{data?.currencies[0].code}</span>
+              </h3>
+              <h3>
+                Borders: <span>{data.borders?.join(', ')}</span>
+              </h3>
+            </div>
+          ) : (
+            ''
+          )}
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
